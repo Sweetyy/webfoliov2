f342aa4509e9d41c561a9732a0fa6a1ec12df6ae
Sweetyy <anthony.gallippi1@gmail.com>
2017-08-22 20:41:41 +0200

    Changed design for works & skills

--- src/app/views/works/works.component.ts
+++ src/app/views/works/works.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksComponent implements OnInit {
 
-  constructor() { }
+  works: any[];
+  constructor() {
+    this.works=[
+      {name: 'Angular 2', images: 'angular_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'},
+      {name: 'CSS3', images: 'css3_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'},
+      {name: 'HTML5', images: 'html5_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'},
+      {name: 'Responsive web design', images: 'responsive_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'},
+      {name: 'Javascript', images: 'js_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'},
+      {name: 'Git', images: 'git_shadow.png', description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'}
+    ]
+  }
 
   ngOnInit() {
   }
