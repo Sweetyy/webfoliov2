cc42dac3280b28e9f7d55f631f5b025cf1025faa
Anthony <anthony.gallippi@safety-line.fr>
2017-09-13 17:31:35 +0200

    Started to work on about view

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -6,7 +6,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  experiences: any[];
+  constructor() {
+    this.experiences=[
+      {
+        name: 'Developer Front-End', 
+        date: '2014 - Now', 
+        description: 'Angular is a free and open-source Javascript structural framework for dynamic web apps. Developped by Google, it lets you use HTML as your template language and lets you extend HTML\'s syntax to express your application\'s components clearly and succinctly.',
+        bcolor: 'lightseagreen',
+        tasks: [
+          {
+            description: 'Design'
+          },
+          {
+            description: 'Coding'
+          }
+        ]
+      },
+      {
+        name: 'Student',
+        date: '2009 - 2014',
+        description: 'Angular is a free and open-source Javascript structural framework for dynamic web apps.',
+        bcolor: 'siver',
+        tasks: [
+          {
+            description: 'Learning'
+          },
+          {
+            description: 'Homework'
+          },
+          {
+            description: 'Cinema'
+          },
+          {
+            description: 'Friend'
+          }
+        ]
+      },      
+    ]
+  }
 
   ngOnInit() {
   }
--- src/app/views/works/works.component.ts
+++ src/app/views/works/works.component.ts
@@ -14,19 +14,22 @@ export class WorksComponent implements OnInit {
         name: 'Dailyrious', 
         images: 'dailyrious.png', 
         link: 'http://dailyrious.net', 
-        description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'
+        description: 'Paranormal Community. Different paranormal themes, mysterious and terrifying every day! Aliens, Dreams, Superhumans, Ghosts, Monsters, Magic and many others. Tell us your stories, share your experience and learn about others. Realized in team of 3 developers.',
+        technologies: '<b>Angular 4, HTML, CSS</b>'
       },
       {
         name: 'Foodtrack', 
         images: 'foodtrack.png', 
         link: 'https://play.google.com/store/apps/details?id=com.ionicframework.final994902', 
-        description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'
+        description: 'Hard to know what and where to eat with your colleagues or friends? Foodtrack is the application you need. Find the place you want to eat in three easy steps and visualize your final choice in Google Maps. Realized with a friend.',
+        technologies: '<b>Angular 2, Ionic 2, HTML, CSS</b>'
       },
       {
         name: 'Just Square', 
         images: 'justsquare.png', 
         link: '', 
-        description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop.'
+        description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop. Realized with some friends.',
+        technologies: '<b>Socket, BabylonJS, HTML, CSS</b>'
       },
     ]
   }
